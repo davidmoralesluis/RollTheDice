ecc7e8651b046e72a66fc9b4e58e42680bdec3af
DavidMacBook <davidmoralesluis@gmail.com>
2022-10-11 09:13:15 +0200

    ToDo 2

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -16,14 +16,23 @@
 
 package com.example.android.diceroller
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
 
         // TODO (02) Use findViewById to get a reference to the button
         // and assign it to an immutable variable called rollButton
